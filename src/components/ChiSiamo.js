ce7ba46247b487b57f9f02a800ba85b4cb087088
maurizioweb <lisa.mau@libero.it>
2023-11-23 08:11:56 +0100

    Descrizione delle modifiche apportate

--- src/components/ChiSiamo.js
+++ src/components/ChiSiamo.js
@@ -17,7 +17,7 @@ const ChiSiamo = () => {
     <Box sx={{ padding: '4rem 0', backgroundColor: '#f5f5f5' }}>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
-          {/* Colonna per titoli e pacheloader */}
+          {/* Colonna per titoli e immagine */}
           <Grid item xs={12} md={6}>
             <motion.div
               initial="hidden"
@@ -38,13 +38,15 @@ const ChiSiamo = () => {
                   backgroundColor: 'grey',
                 }}
               >
-                {/* Qui inserisci il pacheloader */}
+                {/* Qui inserisci l'immagine */}
+                <img src="varnelli.jpg" alt="Descrizione dell'immagine" style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
               </Box>
             </motion.div>
           </Grid>
           {/* Colonna per il paragrafo */}
           <Grid item xs={12} md={6}>
-            <motion.div
+            {/* ... Il resto del codice del testo ... */}
+			 <motion.div
               initial="hidden"
               whileInView="visible"
               viewport={{ once: true }}
@@ -53,11 +55,14 @@ const ChiSiamo = () => {
             >
               <Box sx={{ paddingTop: '9rem' }}> {/* Aumenta il padding per allineare con l'inizio del pacheloader */}
                 <Typography variant="body1" color="textSecondary">
-                  Siamo un team di professionisti appassionati con l'obiettivo di creare esperienze digitali 
-                  indimenticabili. Il nostro focus è sulla qualità, l'innovazione e la velocità per portare 
-                  soluzioni eccellenti ai nostri clienti. Siamo dedicati alla continua ricerca dell'eccellenza 
-                  nel design, nello sviluppo e nella strategia digitale, lavorando a stretto contatto con i nostri 
-                  clienti per realizzare progetti su misura che rispondano perfettamente alle loro esigenze.
+
+"Siamo un team di professionisti culinari appassionati, uniti dalla missione di offrire esperienze gastronomiche eccezionali e indimenticabili. Nel nostro ristorante, la qualità è al centro di tutto ciò che facciamo, dall'accurata selezione degli ingredienti alla cura nella preparazione di ogni piatto. Ci impegniamo nell'innovazione, sperimentando costantemente nuove tecniche culinarie e combinazioni di sapori per sorprendere e deliziare i nostri ospiti.
+
+La nostra cucina si fonda sulla velocità nel servire piatti squisiti, senza mai compromettere la qualità. Ogni membro del nostro team è dedicato a fornire un servizio eccellente, assicurando che ogni esperienza presso il nostro ristorante sia unica e memorabile. Collaboriamo a stretto contatto con fornitori locali per garantire che ogni ingrediente rispecchi freschezza e sostenibilità, rafforzando il nostro impegno verso la qualità e l'ambiente.
+
+Ci dedichiamo alla continua ricerca dell'eccellenza non solo nella cucina ma anche nell'ambiente e nel servizio. La nostra strategia si concentra sulla creazione di un'atmosfera accogliente e rilassante, dove ogni cliente può sentirsi a casa. Lavoriamo insieme per realizzare esperienze personalizzate, rispondendo con attenzione alle preferenze e alle esigenze di ciascuno dei nostri ospiti.
+
+Nel nostro ristorante, ogni pasto è un'opera d'arte, e ogni visita è un'avventura nel mondo dei sapori. Ci impegniamo a mantenere elevati standard di eccellenza e innovazione per rendere ogni momento trascorso con noi un'esperienza indimenticabile e arricchente. Venite a scoprire la passione e la maestria che si celano dietro ogni nostro piatto, e lasciatevi trasportare in un viaggio culinario che stimolerà tutti i vostri sensi."
                 </Typography>
               </Box>
             </motion.div>
--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -18,36 +18,50 @@ const TestimonialSlider = () => {
   ];
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      overflowX: 'auto',
-      padding: '0.5rem',
-      margin: '0'
-    }}>
-      {testimonials.map((testimonial, index) => (
-        <div key={index} style={{
-          flex: 'none',
-          width: '300px',
-          margin: '0.5rem',
-          padding: '1rem',
-          border: '1px solid #ccc',
-          borderRadius: '4px',
-          transition: 'transform 0.3s ease, background-color 0.3s ease', // Transizione fluida per gli effetti
-          backgroundColor: '#fff', // Colore di sfondo iniziale
-          cursor: 'pointer', // Cambia il cursore a pointer per indicare che è interattivo
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' // Ombra leggera per dare profondità
-        }}
-        onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'} // Ingrandisce leggermente la card
-        onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'} // Ritorna alla dimensione originale
-        onMouseEnter={(e) => e.currentTarget.style.backgroundColor = '#f7f7f7'} // Cambia colore di sfondo
-        onMouseLeave={(e) => e.currentTarget.style.backgroundColor = '#fff'} // Ritorna al colore di sfondo originale
-        >
-          <p style={{ marginBottom: '0.5rem' }}>{testimonial.text}</p>
-          <p style={{ textAlign: 'right', fontWeight: 'bold' }}>- {testimonial.author}</p>
-        </div>
-      ))}
+    <div>
+      <h2 style={{
+        textAlign: 'center',
+        margin: '1rem 0' // Aggiungi margini secondo necessità
+		
+
+      }}>
+        Dicono di Noi
+      </h2>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflowX: 'auto',
+        padding: '0.5rem',
+        margin: '0'
+      }}>
+        {testimonials.map((testimonial, index) => (
+          <div key={index} style={{
+            flex: 'none',
+            width: '300px',
+            height: '200px',
+            margin: '0.5rem',
+            padding: '1rem',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            transition: 'transform 0.3s ease, background-color 0.3s ease',
+            backgroundColor: '#fff',
+            cursor: 'pointer',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+          }}
+          onMouseEnter={(e) => {
+            e.currentTarget.style.transform = 'scale(1.05)';
+            e.currentTarget.style.backgroundColor = '#f7f7f7';
+          }}
+          onMouseLeave={(e) => {
+            e.currentTarget.style.transform = 'scale(1)';
+            e.currentTarget.style.backgroundColor = '#fff';
+          }}>
+            <p style={{ marginBottom: '0.5rem' }}>{testimonial.text}</p>
+            <p style={{ textAlign: 'right', fontWeight: 'bold' }}>- {testimonial.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
